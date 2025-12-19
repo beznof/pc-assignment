1471182e594532440d2cb4f6c1d31057c364b610
beznof <viliusviskantas@gmail.com>
2025-12-19 21:33:19 +0200

    Setup Radix-UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,20 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Theme, ThemePanel} from "@radix-ui/themes";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Theme>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo"/>
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react" alt="React logo"/>
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -28,7 +29,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+      <ThemePanel/>
+    </Theme>
   )
 }
 
